97c720a0ea0e4ce1971fcb463ed1cfafb5c1e52e
Soumesh <soumeshkumar57@gmail.com>
2021-08-27 19:10:05 +0530

    Update SQLDatabase

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ from flask_ckeditor import CKEditor, CKEditorField
 from datetime import datetime
 from flask_login import login_user, login_required, LoginManager, current_user, logout_user, UserMixin, \
     AnonymousUserMixin
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
